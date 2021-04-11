7d7f4a5fe2cca2e93954c1da94d8bd9bfd99de6a
KoncordW <kenji.wong1995@gmail.com>
2021-04-11 17:10:15 -0500

    Rename app

--- MainApp/MainApp/wsgi.py
+++ MainApp/MainApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ReactDjango project.
+WSGI config for MainApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ReactDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MainApp.settings')
 
 application = get_wsgi_application()
