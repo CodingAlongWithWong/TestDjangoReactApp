10232a2c92e3edf5cbde54bab684be1a41ceb10a
KoncordW <kenji.wong1995@gmail.com>
2021-04-18 23:37:15 -0500

    Create home page

--- MainApp/frontend/src/components/App.js
+++ MainApp/frontend/src/components/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return <h1>{this.props.name}</h1>;
-    }
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 const appDiv = document.getElementById("app");
-render(<App name="Kenji Test"/>, appDiv)
+render(<App />, appDiv);
